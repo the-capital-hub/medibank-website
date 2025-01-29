011bc54229b9b71fd8ec3d3ddc5d7248ef785dc1
AmanDwivedi9335 <amandwdi@gmail.com>
2025-01-29 11:16:27 +0530

    export out

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    output: 'export', // Enable static export
+    images: {
+      unoptimized: true, // Disables next/image optimization for static export
+    },
+  };
 
 export default nextConfig;
