bdc19df43344b2c4eeec3f9a9f6fa4728442423f
NitinJajoria <95345434+NitinJajoria@users.noreply.github.com>
2025-02-03 11:27:45 +0530

    user data to sheet

--- src/components/ui/SignupPopup.jsx
+++ src/components/ui/SignupPopup.jsx
@@ -21,7 +21,7 @@ export default function SignupPopup({ isOpen, setIsOpen }) {
 		setError(null);
 
 		try {
-			const response = await fetch("/api/signup", {
+			const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/user/addUserdataToSheet`, {
 				method: "POST",
 				headers: {
 					"Content-Type": "application/json",
